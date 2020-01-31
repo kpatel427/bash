cf9e49d216a1eaf3b03290b61b687b3abe81e494
Khushbu Patel <kpatel427@gatech.edu>
2020-01-31 12:06:53 -0500

    update

--- samtoolsDepth.sh
+++ samtoolsDepth.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-
-#echo "Hello!"
+# script to iterate through a list of bam files and get average depth in the region of interest using samtools depth
+# results in a text file with average depth for each bam file
 
 while read p; do
-  echo "samtools depth -r chr2:16080559-16087129 /mnt/isilon/maris_lab/target_nbl_ngs/CellLineExome/aligned_2019/${p}.sorted.reheader.bam  |  awk '{sum+=$3} END { print "$p Average = ",sum/NR}' >> mycn_depths.txt"
-  samtools depth -r chr2:16080559-16087129 /mnt/isilon/maris_lab/target_nbl_ngs/CellLineExome/aligned_2019/${p}.sorted.reheader.bam  |  awk '{sum+=$3} END { print  "'$p' Average = ",sum/NR}' >> mycn_depths.txt
+  echo "samtools depth -r chr2:16080559-16087129 /path/to/bam/${p}.sorted.reheader.bam  |  awk '{sum+=$3} END { print "$p Average = ",sum/NR}' >> depths.txt"
+  samtools depth -r chr2:16080559-16087129 /path/to/bam/${p}.sorted.reheader.bam  |  awk '{sum+=$3} END { print  "'$p' Average = ",sum/NR}' >> depths.txt
   echo "done - $p"
-done </mnt/isilon/maris_lab/target_nbl_ngs/CellLineExome/aligned_2019/SampleNames.txt
+done </path/to/bam/bamFileNames.txt
