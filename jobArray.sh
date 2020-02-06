e8dd5685c4fdfa1bab158714d102f55307628493
Khushbu Patel <kpatel427@gatech.edu>
2020-02-06 11:41:50 -0500

    jobArray

---
+++ jobArray.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+#$ -j y
+#$ -V
+#$ -t 1-4
+#$ -l m_mem_free=8G,h_vmem=8G
+#$ -pe smp 4
+#$ -cwd
+
+
+
+
+input=("bill" "ted" "lisa" "42")
+
+function echoMe {
+    echo "Hello $1 from job $SGE_TASK_ID" # $SGE_TASK_ID = index value & $1 takes array value
+    sleep 30
+    exit 0
+}
+
+echoMe ${input[$SGE_TASK_ID]}
